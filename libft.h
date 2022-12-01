5838db3a74dadb65b3837c0639a8a04381a47133
fgomez-d <fgomez-d@student.42malaga.com>
2022-12-01 15:10:49 +0100

    Finish strlcat & strlcpy

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: fgomez-d <fgomez-d@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/30 16:12:48 by fgomez-d          #+#    #+#             */
-/*   Updated: 2022/11/30 17:35:42 by fgomez-d         ###   ########.fr       */
+/*   Updated: 2022/12/01 13:22:42 by fgomez-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,5 +31,7 @@ void	*ft_memset(void *s, int c, size_t n);
 void	*ft_bzero(void *s, size_t n);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t n);
+int		ft_strlcpy(char *dest, const char *src, size_t size);
+int		ft_strlcat(char *dest, const char *src, size_t size);
 
 #endif
