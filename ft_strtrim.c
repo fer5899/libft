221166936fb7394b4ad75e87962bfcab710e8493
fgomez-d <fgomez-d@student.42malaga.com>
2022-12-06 14:48:39 +0100

    Fix strtrim using substr

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: fgomez-d <fgomez-d@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/02 14:00:30 by fgomez-d          #+#    #+#             */
-/*   Updated: 2022/12/02 18:07:35 by fgomez-d         ###   ########.fr       */
+/*   Updated: 2022/12/06 14:47:33 by fgomez-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,19 +14,19 @@
 
 char	*ft_strtrim(char const *s1, char const *set)
 {
+	char	*tstr;
 	int		start;
 	int		end;
-	char	*tstr;
 
 	start = 0;
 	end = ft_strlen(s1);
-	while (ft_strchr(set, s1[start]) != NULL && s1[start] != '\0')
-		start++;
-	while (ft_strchr(set, s1[end]) != NULL && end > 0 && end > start)
-		end--;
-	tstr = (char *) ft_calloc(end - start + 2, sizeof(char));
-	if (tstr == NULL)
-		return (NULL);
-	ft_strlcpy(tstr, s1 + start, end - start + 2);
+	if (ft_strncmp(set, "", 1))
+	{
+		while (ft_strchr(set, s1[start]) != NULL && s1[start] != '\0')
+			start++;
+		while (ft_strchr(set, s1[end]) != NULL && end > 0 && end > start)
+			end--;
+	}
+	tstr = ft_substr(s1, start, end - start + 1);
 	return (tstr);
 }
