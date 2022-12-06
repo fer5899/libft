332aa2fc65e1e2f5302c66be08e06f8e2a205d83
fgomez-d <fgomez-d@student.42malaga.com>
2022-12-06 15:12:14 +0100

    Fix lstmap for strict mode

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,11 +6,19 @@
 /*   By: fgomez-d <fgomez-d@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/06 13:39:20 by fgomez-d          #+#    #+#             */
-/*   Updated: 2022/12/06 14:09:21 by fgomez-d         ###   ########.fr       */
+/*   Updated: 2022/12/06 15:10:57 by fgomez-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+static void	*plstnew(t_list	**nl, t_list **nn, void **nc, void (*d)(void *))
+{
+	ft_lstclear(nl, d);
+	free(*nn);
+	free(*nc);
+	return (NULL);
+}
 
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
@@ -25,11 +33,12 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 		if (newcont == NULL)
 		{
 			ft_lstclear(&newlst, del);
+			free(newcont);
 			break ;
 		}
 		newnode = ft_lstnew(newcont);
 		if (newnode == NULL)
-			return (NULL);
+			return (plstnew(&newlst, &newnode, &newcont, del));
 		ft_lstadd_back(&newlst, newnode);
 		lst = lst->next;
 	}
