be7b600ff588a8e5fb872a99fef218d74ac75bb6
fgomez-d <fgomez-d@student.42malaga.com>
2022-12-06 14:49:34 +0100

    Fix strjoin

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: fgomez-d <fgomez-d@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/02 13:37:46 by fgomez-d          #+#    #+#             */
-/*   Updated: 2022/12/02 14:19:16 by fgomez-d         ###   ########.fr       */
+/*   Updated: 2022/12/06 14:17:23 by fgomez-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,13 @@
 char	*ft_strjoin(char const *s1, char const *s2)
 {
 	char	*catstr;
+	int		s1len;
 
-	catstr = (char *) calloc(ft_strlen(s1) + ft_strlen(s2), sizeof(char));
+	s1len = ft_strlen(s1);
+	catstr = (char *) ft_calloc(s1len + ft_strlen(s2) + 1, sizeof(char));
 	if (catstr == NULL)
 		return (NULL);
-	ft_strlcpy(catstr, s1, ft_strlen(s1));
-	ft_strlcat(catstr, s2, ft_strlen(s1) + ft_strlen(s2));
+	ft_strlcpy(catstr, s1, s1len + 1);
+	ft_strlcat(catstr, s2, s1len + ft_strlen(s2) + 1);
 	return (catstr);
 }
