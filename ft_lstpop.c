27951ac3bc1c127d44e7ab1c6381349829c4f047
fgomez-d <fgomez-d@student.42malaga.com>
2023-09-26 16:46:40 +0200

    Fix lstpop

--- ft_lstpop.c
+++ ft_lstpop.c
@@ -6,7 +6,7 @@
 /*   By: fgomez-d <fgomez-d@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/15 16:06:11 by fgomez-d          #+#    #+#             */
-/*   Updated: 2023/07/17 23:07:36 by fgomez-d         ###   ########.fr       */
+/*   Updated: 2023/09/26 16:44:23 by fgomez-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,15 @@
 
 t_list	*ft_lstpop(t_list **lst, t_list *to_pop)
 {	
+	t_list	*orig;
+
 	if (*lst == NULL || to_pop == NULL)
 		return (NULL);
+	orig = *lst;
 	if ((*lst) == to_pop)
 	{
 		(*lst) = (*lst)->next;
+		to_pop->next = NULL;
 		return (to_pop);
 	}
 	while ((*lst) != NULL)
@@ -27,6 +31,7 @@ t_list	*ft_lstpop(t_list **lst, t_list *to_pop)
 		{
 			(*lst)->next = (*lst)->next->next;
 			to_pop->next = NULL;
+			*lst = orig;
 			return (to_pop);
 		}
 		(*lst) = (*lst)->next;
