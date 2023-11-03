504b8b5788261f71e7def5e166728dd135acd263
fgomez-d <fgomez-d@student.42malaga.com>
2023-11-03 12:41:49 +0100

    Add comment

--- ft_abs.c
+++ ft_abs.c
@@ -6,12 +6,15 @@
 /*   By: fgomez-d <fgomez-d@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/12 16:50:21 by fgomez-d          #+#    #+#             */
-/*   Updated: 2023/04/12 16:51:18 by fgomez-d         ###   ########.fr       */
+/*   Updated: 2023/11/03 12:41:31 by fgomez-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+/*
+This is a function for getting the absolute value of an integer
+*/
 int	ft_abs(int n)
 {
 	if (n < 0)
